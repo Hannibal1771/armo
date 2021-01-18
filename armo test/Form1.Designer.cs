98b8d7d84bded9c561f8c26ab71855d94eeeb680
Hannibal1771 <36897389+Hannibal1771@users.noreply.github.com>
2021-01-18 04:33:53 +0300

    Add files via upload

---
+++ armo test/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace ARMO_test_task
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSelDir = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonSelDir
+            // 
+            this.buttonSelDir.Location = new System.Drawing.Point(450, 55);
+            this.buttonSelDir.Name = "buttonSelDir";
+            this.buttonSelDir.Size = new System.Drawing.Size(237, 23);
+            this.buttonSelDir.TabIndex = 0;
+            this.buttonSelDir.Text = "Переход к выбранной директории";
+            this.buttonSelDir.UseVisualStyleBackColor = true;
+            this.buttonSelDir.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 56);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(432, 382);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.AfterExpand += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterExpand);
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(132, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(555, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Текущая директория:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(450, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Общее количество файлов:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(450, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Общее количество папок:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(498, 252);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(236, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(541, 278);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Отмена поиска";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(563, 278);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Поиск";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(450, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(171, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Количество найденных файлов: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(450, 255);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Поиск:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.buttonSelDir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSelDir;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ armo test/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ARMO_test_task
+{
+    
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            treeView1.AfterExpand += treeView1_AfterExpand;
+            treeView1.AfterSelect += treeView1_AfterSelect;
+        }
+
+        private void Count()
+        {
+            int x = 0;
+            int x1 = 0;
+            int y = 0;
+            x = PC + PC1;
+            x1 = FC + FC1;
+            y = x + x1;
+            label3.Text = $"Общее количество папок в текущей директории: {x}";
+            label2.Text = $"Общее количество файлов в текущей директории: {x1}";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            treeView1.Nodes.Clear();         
+            TreeNode tr = new TreeNode { Text = textBox1.Text };
+            GetFolders(textBox1.Text, tr);
+            treeView1.Nodes.Add(tr);
+            Count();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult dr =  fbd.ShowDialog();
+            if(dr == DialogResult.OK)
+            {
+                
+                TreeNode tr = new TreeNode { Text = fbd.SelectedPath};
+                GetFolders(fbd.SelectedPath,tr);
+                treeView1.Nodes.Add(tr);
+                Count();
+            }
+        }
+
+        int PC = 0;
+        int FC = 0;
+        private void FillTree(string path, TreeNode NewNode)
+        {
+            try
+            {
+                PC = 0;
+                string[] dir = Directory.GetDirectories(path);
+                for (int i = 0; i < dir.Length; i++)
+                {
+                    PC++;
+                    TreeNode tr = new TreeNode { Text = dir[i] };
+                    tr.Text = dir[i].Remove(0, dir[i].LastIndexOf("\\") + 1);
+                    NewNode.Nodes.Add(tr);
+                }
+                FC = 0;
+                string[] dirF = Directory.GetFiles(path);
+                for (int i = 0; i < dirF.Length; i++)
+                {
+                    FC++;
+                    TreeNode trF = new TreeNode { Text = dirF[i] };
+                    trF.Text = dirF[i].Remove(0, dir[i].LastIndexOf("\\") + 1);
+                    NewNode.Nodes.Add(trF);
+                }
+            }
+            catch (Exception ex) { }
+           
+        }
+        int PC1 = 0;
+        int FC1 = 0;
+        private void GetFolders(string path, TreeNode Anode)
+        {
+            try
+            {
+                PC1 = 0;
+                string[] dir = Directory.GetDirectories(path);
+                for (int i = 0; i < dir.Length; i++)
+                {
+                    PC1++;
+                    TreeNode tr = new TreeNode { Text = dir[i] };
+                    tr.Text = dir[i].Remove(0, dir[i].LastIndexOf("\\") + 1);
+                    FillTree(dir[i], tr);
+                    Anode.Nodes.Add(tr);
+                }
+                FC1 = 0;
+                string[] dirF = Directory.GetFiles(path);
+                for (int i = 0; i < dirF.Length; i++)
+                {
+                    FC1++;
+                    TreeNode trF = new TreeNode { Text = dirF[i] };
+                    trF.Text = dirF[i].Remove(0, dirF[i].LastIndexOf("\\") + 1);
+                    FillTree(dirF[i], trF);
+                    Anode.Nodes.Add(trF);
+                }
+                
+            }
+            catch (Exception ex) { }
+           
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            textBox1.Text = e.Node.FullPath;
+           
+        }
+
+        private void treeView1_AfterExpand(object sender, TreeViewEventArgs e)
+        {
+            textBox1.Text = e.Node.FullPath;
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Cancel();
+            treeView1.Nodes.Clear();
+            textBox1.Text = "";
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult dr = fbd.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+
+                TreeNode tr = new TreeNode { Text = fbd.SelectedPath };
+                GetFolders(fbd.SelectedPath, tr);
+                treeView1.Nodes.Add(tr);
+                Count();
+                
+            }
+        }
+        List<TreeNode> SearchedNodes = new List<TreeNode>();
+        string LastSearch;
+
+        private void Find(string stext, TreeNode sNode)
+        {
+            while(sNode != null)
+            {
+                if (sNode.Text.Contains(stext))
+                {
+                    SearchedNodes.Add(sNode);
+                }
+
+                if (sNode.Nodes.Count != 0)
+                {
+                    Find(stext, sNode.Nodes[0]);
+                }
+                sNode = sNode.NextNode;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Cancel();
+            
+        }
+
+        private void Cancel()
+        {
+            try
+            {
+                label4.Text = $"Количество найденных файлов: {0}";
+                textBox2.Text = "";
+                if (SearchedNodes.Count > 0)
+                {
+                    for (int i = 0; i < SearchedNodes.Count; i++)
+                    {
+                        TreeNode tr = SearchedNodes[i];
+                        treeView1.SelectedNode = tr;
+                        treeView1.SelectedNode.Collapse();
+                        treeView1.SelectedNode.BackColor = treeView1.BackColor;
+                        treeView1.Select();
+
+                    }
+                }
+                button3.Visible = true;
+                button2.Visible = false;
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox2.Text.Length > 0)
+                {
+                    if (LastSearch != textBox2.Text)
+                    {
+                        
+                        SearchedNodes.Clear();
+                        LastSearch = textBox2.Text;
+                        Find(textBox2.Text, treeView1.Nodes[0]);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Пустое значение");
+                    button2.Visible = false;
+                    button3.Visible = true;
+                }
+                int i = 0;
+                if (SearchedNodes.Count > 0)
+                {
+                    for (; i < SearchedNodes.Count; i++)
+                    {
+                        TreeNode tr = SearchedNodes[i];
+                        treeView1.SelectedNode = tr;
+                        treeView1.SelectedNode.Expand();
+                        treeView1.SelectedNode.BackColor = Color.Red;
+                        treeView1.Select();
+                    }
+                    label4.Text = $"Количество найденных файлов: {i}";
+                    button2.Visible = true;
+                    button3.Visible = false;
+                }
+            }
+            catch (Exception ex) { }
+           
+        }
+    }
+}
